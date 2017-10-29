c0aa64a9e20bbd32a48d493c8697990f8c45599e
alexstar2008 <alexstar2008@ukr.net>
2017-10-29 10:35:59 +0200

    X-Response-Time Header

--- config/express.js
+++ config/express.js
@@ -3,7 +3,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const compression = require('compression');
-
+const responseTime = require('response-time');
 const logger = require('./winston');
 const routes = require('../routes/index.route');
 
@@ -11,6 +11,8 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(compression());
+app.use(responseTime());
+// X-Response-Time
 
 app.use('/api', routes);
 app.use((err, req, res) => {
