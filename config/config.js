c340458cf4d33aa98acf4ed1470a4ec86ad57524
OleksiiStarovoitneko <alexey.starovoytenko@igniteoutsourcing.com>
2018-01-18 14:55:44 +0200

    removed local env vars

--- config/config.js
+++ config/config.js
@@ -2,22 +2,22 @@
 
 const dbSettings = {
 	mongoDb: {
-		db: process.env.MONGO_DB || 'bizguid',
-		username: process.env.MONGO_DB_USERNAME || 'admin',
-		pass: process.env.MONGO_DB_PASS || '4232',
-		host: process.env.MONGO_DB_HOST || 'ds143532.mlab.com',
-		port: process.env.MONGO_PORT || 43532
+		db: process.env.MONGO_DB
+		username: process.env.MONGO_DB_USERNAME,
+		pass: process.env.MONGO_DB_PASS,
+		host: process.env.MONGO_DB_HOST,
+		port: process.env.MONGO_PORT
 	},
 	sqlDb: {
-		db: process.env.SQL_DB || 'new_biz_guid',
-		username: process.env.SQL_DB_USERNAME || 'root',
-		pass: process.env.SQL_DB_PASS || '970619',
-		host: process.env.SQL_DB_HOST || '127.0.0.1',
-		port: process.env.SQL_PORT || 3306
+		db: process.env.SQL_DB,
+		username: process.env.SQL_DB_USERNAME,
+		pass: process.env.SQL_DB_PASS,
+		host: process.env.SQL_DB_HOST,
+		port: process.env.SQL_PORT
 	}
 };
 const serverSettings = {
-	port: process.env.PORT || 3001
+	port: process.env.PORT
 };
 
 module.exports = {dbSettings, serverSettings};
